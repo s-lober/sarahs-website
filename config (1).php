6bb08ce85cb4c203bca94fe74947d93e86639f7a
s-lober <sarah@ohana.org>
2016-10-08 11:22:55 -0300

    Delete config (1).php

--- config (1).php
+++
@@ -1,7 +0,0 @@
-<?php
-	$GLOBALS["hostname"] = "ohana.org";
-	$GLOBALS["port"] = 2078;
-	$GLOBALS["username"] = "pevszhpa_sarah";
-	$GLOBALS["password"] = "w2C!N*fneI";
-	$GLOBALS["database"] = "pevszhpa_sarahs_blog";
-?>
